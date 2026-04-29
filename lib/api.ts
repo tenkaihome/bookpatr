1d9d9187794f61987f052d1ee5db345543634a40
lichbich <tronglich.a2@gmail.com>
2026-04-30 01:16:39 +0700

    update api

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://logbook-kohl-one.vercel.app/api';
 
 export interface Book {
   id: string;
